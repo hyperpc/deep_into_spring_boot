e1696deb93e8b7870a311c1eecbc082bf382374a
Hou Yang Po (NCS) <yangpo@ncsi.com.cn>
2022-04-02 23:20:47 +0800

    study note of ch02 data

---
+++ ch02_data/hyp-data/src/main/java/com/hyp/data/hypdata/HypDataApplication.java
@@ -0,0 +1,13 @@
+package com.hyp.data.hypdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HypDataApplication.class, args);
+	}
+
+}
---
+++ ch02_data/hyp-data/src/test/java/com/hyp/data/hypdata/HypDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyp.data.hypdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
